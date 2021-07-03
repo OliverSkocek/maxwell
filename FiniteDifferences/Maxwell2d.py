738289b4d3a2952d9bbbf18555f3ce81a6857786
OliverSkocek <oliverskc@protonmail.com>
2021-07-03 09:21:35 +0200

    started 2d maxwell fd class.

---
+++ FiniteDifferences/Maxwell2d.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+from tensorflow.nn import convolution
+
+from celluloid import Camera
+from matplotlib import animation, rc
+from IPython.display import HTML
+from IPython.display import clear_output
+from time import time
+
+
+class FiniteDifferenceMaxwell:
+
+    def __init__(self, ):
+        pass
