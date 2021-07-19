a99b1afe5a03ca3c3fb0e763c171095828ebe24f
OliverSkocek <oliverskc@protonmail.com>
2021-07-19 16:35:48 +0200

    E <- U

--- FiniteDifferences/Maxwell2d.py
+++ FiniteDifferences/Maxwell2d.py
@@ -22,13 +22,13 @@ class Maxwell2DFiniteDifference:
     Solver for the 2 dimensional Maxwell equations.
     """
 
-    def __init__(self, mesh_size, step_size, geometry=None, resistance=None, permitivity=None, permeability=None,
+    def __init__(self, mesh_size, step_size, geometry=None, conductivity=None, permitivity=None, permeability=None,
                  frame_rate=8, diff_type: DifferenceType = DifferenceType.CENTRAL_DIFFERENCE):
         self.mesh_size = mesh_size
         self.step_size = step_size
         self.geometry = geometry
         self._number_divisions_per_axis = int(1 / self.mesh_size)
-        self._U = np.zeros(shape=(self._number_divisions_per_axis, self._number_divisions_per_axis, 2))
+        self._E = np.zeros(shape=(self._number_divisions_per_axis, self._number_divisions_per_axis, 2))
         self._I = np.zeros(shape=(self._number_divisions_per_axis, self._number_divisions_per_axis, 2))
         self._B = np.zeros(shape=(self._number_divisions_per_axis, self._number_divisions_per_axis, 1))
         self._p = np.zeros(shape=(self._number_divisions_per_axis, self._number_divisions_per_axis, 1))
@@ -36,10 +36,10 @@ class Maxwell2DFiniteDifference:
         axis_diskrete = np.linspace(0, 1, self._number_divisions_per_axis)
         mesh = np.stack(np.meshgrid(axis_diskrete, axis_diskrete))
 
-        if resistance:
-            self._R = np.vectorize(resistance)(*mesh)
+        if conductivity:
+            self._g = np.vectorize(conductivity)(*mesh)
         else:
-            self._R = np.inf * np.ones(
+            self._g = np.zeros(
                 shape=(self._number_divisions_per_axis, self._number_divisions_per_axis, 1))
 
         if permitivity:
@@ -59,18 +59,18 @@ class Maxwell2DFiniteDifference:
             X = np.array([[0, 1, 0], [0, 0, 0], [0, -1, 0]])
             Y = np.array([[0, 0, 0], [-1, 0, 1], [0, 0, 0]])
             self._faraday_filter = np.stack([X, Y]).reshape((2, 3, 3, 1)).transpose(1, 2, 0,
-                                                                                    3) / self.mesh_size ** 2 / 2
-            self._ampere_filter = np.stack([X, Y]).reshape((2, 3, 3, 1)).transpose(1, 2, 3, 0) / 2
+                                                                                    3) / self.mesh_size / 2
+            self._ampere_filter = np.stack([X, Y]).reshape((2, 3, 3, 1)).transpose(1, 2, 3, 0) / self.mesh_size / 2
         else:
             X = np.array([[0, 0, 0], [1, -1, 0], [0, 0, 0]])
             Y = np.array([[0, 1, 0], [0, -1, 0], [0, 0, 0]])
             self._continuity_filter = np.stack([X, Y]).reshape((2, 3, 3, 1)).transpose(1, 2, 0, 3)
             X = np.array([[0, 0, 0], [0, 1, 0], [0, -1, 0]])
             Y = np.array([[0, 0, 0], [0, -1, 1], [0, 0, 0]])
-            self._faraday_filter = np.stack([X, Y]).reshape((2, 3, 3, 1)).transpose(1, 2, 0, 3) / self.mesh_size ** 2
+            self._faraday_filter = np.stack([X, Y]).reshape((2, 3, 3, 1)).transpose(1, 2, 0, 3) / self.mesh_size
             X = np.array([[0, 1, 0], [0, -1, 0], [0, 0, 0]])
             Y = np.array([[0, 0, 0], [-1, 1, 0], [0, 0, 0]])
-        self._ampere_filter = np.stack([X, Y]).reshape((2, 3, 3, 1)).transpose(1, 2, 3, 0)
+        self._ampere_filter = np.stack([X, Y]).reshape((2, 3, 3, 1)).transpose(1, 2, 3, 0) / self.mesh_size
 
         self.fig, self.axs = (None, None)
         self.camera = None
@@ -79,7 +79,7 @@ class Maxwell2DFiniteDifference:
 
     def evolve(self, initial_B, initial_E, initial_charge, integration_period, order=1, video=False):
         """
-        Evolves an initial state of the electromagnetic field over a time interval.
+        Evolves an initial state of the electromagnetic field over a time interval "integration_period".
 
         :param initial_B: initial magnetic field, callable (x,y) -> B.
         :param initial_E: initial electric field, callable (x,y) -> (Ex,Ey)
@@ -99,16 +99,16 @@ class Maxwell2DFiniteDifference:
         axis_diskrete = np.linspace(0, 1, self._number_divisions_per_axis)
         mesh = np.stack(np.meshgrid(axis_diskrete, axis_diskrete))
 
-        self._U[:, :, 0] = np.vectorize(lambda x, y: initial_E(x, y)[0])(*mesh) * self.mesh_size
-        self._U[:, :, 1] = np.vectorize(lambda x, y: initial_E(x, y)[1])(*mesh) * self.mesh_size
+        self._E[:, :, 0] = np.vectorize(lambda x, y: initial_E(x, y)[0])(*mesh)
+        self._E[:, :, 1] = np.vectorize(lambda x, y: initial_E(x, y)[1])(*mesh)
         self._B = np.vectorize(initial_B)(*mesh)
         self._p = np.vectorize(initial_charge)(*mesh) * self.mesh_size ** 2
 
         self._B = (2 * self._B + np.flipud(self._B) + np.fliplr(self._B)) / 4
         eps = tf.constant(self._eps.reshape((1, N, N, 1)), name='dielectricity', dtype=tf.float64)
         mu = tf.constant(self._mu.reshape((1, N, N, 1)), name='permitivity', dtype=tf.float64)
-        R = tf.constant(self._R.reshape((1, N, N, 1)), name='resistance', dtype=tf.float64)
-        U = tf.Variable(self._U.reshape((1, N, N, 2)), name='e_field', dtype=tf.float64)
+        g = tf.constant(self._g.reshape((1, N, N, 1)), name='conductivity', dtype=tf.float64)
+        E = tf.Variable(self._E.reshape((1, N, N, 2)), name='e_field', dtype=tf.float64)
         B = tf.Variable(self._B.reshape((1, N, N, 1)), name='b_field', dtype=tf.float64)
         p = tf.Variable(self._p.reshape((1, N, N, 1)), name='charge_density', dtype=tf.float64)
 
@@ -121,22 +121,22 @@ class Maxwell2DFiniteDifference:
 
         for jter in range(int(integration_period / self.step_size)):
             dB = B
-            dU = U
+            dE = E
             for ord in range(1, order + 1):
-                I = dU / R
-                dB, dU = (convolution(dU, filters=faraday_filter, padding='SAME'),
-                          convolution(dB, filters=ampere_filter, padding='SAME') / eps / mu - I / eps)
-                dp = convolution(I, filters=continuity_filter, padding='SAME')
+                j = g * dE
+                dB, dE = (convolution(dE, filters=faraday_filter, padding='SAME'),
+                          convolution(dB, filters=ampere_filter, padding='SAME') / eps / mu - j / eps)
+                dp = convolution(j * self.mesh_size, filters=continuity_filter, padding='SAME')
 
-                U.assign_add(dU * self.step_size ** ord / factorial(ord))
+                E.assign_add(dE * self.step_size ** ord / factorial(ord))
                 B.assign_add(dB * self.step_size ** ord / factorial(ord))
                 p.assign_add(dp * self.step_size ** ord / factorial(ord))
             if video and (jter % video_period == 0):
-                self._record(p / self.mesh_size ** 2, U / R, U / self.mesh_size, B)
+                self._record(p / self.mesh_size ** 2, g * E * self.mesh_size, E, B)
 
         if video:
             self.generate_mp4()
-        return tf.squeeze(B).numpy(), tf.squeeze(U).numpy() / self.mesh_size, tf.squeeze(U / R).numpy(), tf.squeeze(
+        return tf.squeeze(B).numpy(), tf.squeeze(E).numpy(), tf.squeeze(g * E * self.mesh_size).numpy(), tf.squeeze(
             p).numpy() / self.mesh_size ** 2
 
     def _record(self, charge_density, current, electric_field, magnetic_field):
@@ -161,7 +161,7 @@ class Maxwell2DFiniteDifference:
                               np.linspace(0, 1, int(current.shape[0] / arrow_num)),
                               current[::arrow_num, ::arrow_num, 0],
                               current[::arrow_num, ::arrow_num, 1],
-                              scale=0.08)
+                              scale=0.2)
         self.axs[0, 1].set_title('current')
         self.axs[1, 0].imshow(magnetic_field, vmin=-1, vmax=1)
         self.axs[1, 0].set_title('magnetic field')
